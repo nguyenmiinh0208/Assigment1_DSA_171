444fa455f2ff79feb075a5d6eda1878dcfcfcaa3
unknown <nguyenminhnh0208@gmail.com>
2018-08-07 12:25:16 +0700

    official

--- dbLib.cpp
+++ dbLib.cpp
@@ -8,13 +8,8 @@
 *               This file implements functions used for database management
 * =========================================================================================
 */
-<<<<<<< HEAD
-//modified: 07/08/2018 changed 1th
-=======
-//modified: 07/08/2018
->>>>>>> parent of 95360b5... Changed 1th
-#include "dbLib.h"
 
+#include "dbLib.h"
 #include <time.h>
 #include <fstream>
 #include <string>
